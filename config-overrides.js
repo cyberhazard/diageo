4d1a0a4fdd7b3653c9840ab1508a653b3fcbf850
CyberHazard <cyberhazard@inbox.ru>
2018-06-07 23:16:07 +0300

    ADD: Icons

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components');
-const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireSVGR = require('react-app-rewire-svgr');
 
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env)
   config = rewireStyledComponents(config, env);
+  config = rewireSVGR(config, env);
   return config;
 }
