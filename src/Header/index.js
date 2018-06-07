84b6626496b855613c25866f06949af2954aa818
CyberHazard <cyberhazard@inbox.ru>
2018-06-08 00:56:53 +0300

    refactor: replace images

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Header = styled.header`
   grid-column: span 3;
   height: 100%;
-  background: url(${require('./../_assets/images/shelf-main.png')}) left bottom/100% auto no-repeat, url(${require('./../_assets/images/top-bg.png')}) center center/cover;
+  background: url(${require('./../_assets/images/shelf-main.png')}) left bottom/100% auto no-repeat, url(${require('./../_assets/images/top-bg.png')}) left top/cover;
 `;
 
 export default () => (
--- src/Right/index.js
+++ src/Right/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Right = styled.div`
-  background: url(${require('./../_assets/images/right-bg.png')}) center center/100% calc(100% + 3rem) no-repeat;
-  background-position: 0 -3rem;
+  background: url(${require('./../_assets/images/right-bg.jpg')}) center center/100% calc(100% + 2rem) no-repeat;
+  background-position: 0 -2rem;
 `;
 
 export default () => (
